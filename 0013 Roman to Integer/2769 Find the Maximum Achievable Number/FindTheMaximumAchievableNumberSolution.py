82f53a4be98bed83cac1f2a864248be8223ad388
Sarah Baki <sabdu023@uottawa.ca>
2026-07-17 00:40:30 -0400

    Delete 0013 Roman to Integer/2769 Find the Maximum Achievable Number directory

--- 0013 Roman to Integer/2769 Find the Maximum Achievable Number/FindTheMaximumAchievableNumberSolution.py
+++
@@ -1,9 +0,0 @@
-class Solution(object):
-    def theMaximumAchievableX(self, num, t):
-        """
-        :type num: int
-        :type t: int
-        :rtype: int
-        """
-        return (num+2*t)
-        
